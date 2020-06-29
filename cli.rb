fabdb91819da4e14a9224cfefbab3e0b4299bfbf
hadad <hadadus@gmail.com>
2020-06-29 21:30:16 +0200

    Simplest possible CLI

---
+++ cli.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# ruby
+# !/usr/bin/env ruby
+
+# require 'optparse'
+# options = {}
+# OptionParser.new do |parser|
+#   parser.on('-n', '--name NAME', 'The name of the person') do |value|
+#     options[:name] = value
+#   end
+# end.parse!
+# puts "Hello #{options[:name]}"
+puts 'hey'
