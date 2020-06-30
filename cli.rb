fa49d5cd1b8df84fb6ca74ebe3c68988907b30ed
hadad <hadadus@gmail.com>
2020-06-30 22:40:41 +0200

    first basic cli hello world

--- cli.rb
+++ cli.rb
@@ -1,14 +1,12 @@
+#!/usr/bin/env ruby
+# ruby
 # frozen_string_literal: true
 
-# ruby
-# !/usr/bin/env ruby
-
-# require 'optparse'
-# options = {}
-# OptionParser.new do |parser|
-#   parser.on('-n', '--name NAME', 'The name of the person') do |value|
-#     options[:name] = value
-#   end
-# end.parse!
-# puts "Hello #{options[:name]}"
-puts 'hey'
+require 'optparse'
+options = {}
+OptionParser.new do |parser|
+  parser.on('-n', '--name NAME', 'The name of the person') do |value|
+    options[:name] = value
+  end
+end.parse!
+puts "Hello #{options[:name] || 'stanger'}"
